dffe22a3d06d166e38368bdaa97bb85ebdc7cbf8
shivakumar2008 <shivakumar2008@gmail.com>
2022-01-18 23:13:44 +0530

    new design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import linkedin from './icons/linkedin.png';
-import github from './icons/github.png';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub }  from '@fortawesome/free-brands-svg-icons';
+import { faThumbsUp} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <div className="sticky-header">Under Construction... More, more things to come</div>
       <header className="App-header">
         <h1>Shiv Kumar Chintapalli</h1>
+        {/* <FontAwesomeIcon color='white' size='xs' icon={faThumbsUp} /> */}
         <p>Exploring things for <b><em>{lapsedTime.years} years, {lapsedTime.months} months and {lapsedTime.days} days approx. </em></b>
           I am an enthusiast, everything about tech and nature excites me. Although there is a lot to keep in mind, I just keep the references... <em>small memory</em>.
         </p>
@@ -19,7 +21,7 @@ function App() {
       <main className="App-body">
         <section id="skills">
           <div className="main-layout-block" id="left-up">
-            <p>Powers which I used the most</p>
+            <h3>Powers which I used the most</h3>
             <ul>
               <li>Spring boot</li>
               <li>Vaadin</li>
@@ -27,7 +29,7 @@ function App() {
             </ul>
           </div>
           <div className="main-layout-block right-align" id="right-up">
-            <p>Powers sparsely used</p>
+            <h3>Powers sparsely used</h3>
             <ul>
               <li>AI/ML</li>
               <li>Microservices</li>
@@ -38,7 +40,7 @@ function App() {
 
         <section id="learning">
           <div className="main-layout-block" id="left-down">
-            <p>Learning how to use these powers</p>
+            <h3>Learning how to use these powers</h3>
             <ul>
               <li>Angular</li>
               <li>Cloud</li>
@@ -46,7 +48,7 @@ function App() {
             </ul>
           </div>
           <div className="main-layout-block right-align" id="right-down">
-            <p>My Hobbies, when I am not using powers</p>
+            <h3>My Hobbies, when I am not using powers</h3>
             <ul>
               <li>Travelling</li>
               <li>Googleing</li>
@@ -57,9 +59,10 @@ function App() {
       </main>
       <footer>
         <ul className="footer-social-icons">
-          <li><a href="https://www.linkedin.com/in/shiv-kumar-chintapalli/" rel="noreferrer noopener" target="_blank"><img src={linkedin} alt="linkedin redirect" /></a></li>
-          <li><a href="https://github.com/shivakumar2008" rel="noreferrer noopener" target="_blank"><img src={github} alt="github redirect" /></a></li>
+          <li><a href="https://www.linkedin.com/in/shiv-kumar-chintapalli/" rel="noreferrer noopener" target="_blank"><FontAwesomeIcon color='grey' size='lg' icon={faLinkedinIn} /></a></li>
+          <li><a href="https://github.com/shivakumar2008" rel="noreferrer noopener" target="_blank"><FontAwesomeIcon color='grey' size='lg' icon={faGithub} /></a></li>
         </ul>
+        {/* <p className='people-count'>People visited: <span id="visit-count"></span></p> */}
       </footer>
     </div>
   );
